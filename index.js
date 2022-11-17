f4fd76a853b73284bf956ec18eca56021216d382
Jackline <jackline.muthama@student.moringaschool.com>
2022-11-17 17:10:52 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+let name=["Guadalupe", "Ollie", "Aki"]
+function writeCards(name , surprise) {
+    let messages=[]
+    for(let j=0;j<name.length;j++){
+        messages.push(`Thank you, ${name[j]}, for the wonderful surprise gift!`)
+    }
+    return messages
+}
+function countDown(j){
+    for(let n=j;n>=0;n--){
+    console.log(n)    
+    }
+}
